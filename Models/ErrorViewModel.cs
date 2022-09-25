88deb220e71e6461a7b2d6cca6bcdf5c1f07e2f7
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2022-09-25 10:15:27 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Flexbox_Gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
